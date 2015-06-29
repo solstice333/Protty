b39b40d279b8f16137ec89ff3c48a7c9c237b15d
Kevin <knavero@gmail.com>
2015-06-28 18:47:36 -0700

    looks finished

--- protty.py
+++ protty.py
@@ -1,21 +1,26 @@
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
+      # seq   # identifer
+N_ = ("MWT",  "N_")
+P =  ("TRC",  "P")
+pg = ("KWP",  "pg")
 
-A = "1"
-B = "2"
-C = "3"
-
-regions = [A, B, C]
+regions = [N_, P, pg]
 
 beg = 0
 end = len(regions)
 for i in range(end):
    for j in range(beg,end): 
       kitty = ""
-      kitty = kitty.join(regions[beg:j + 1])
-      print(kitty)
-   beg += 1
+      cat = ""
 
+      for k in range(beg, j + 1):
+         kitty += regions[k][0]   # seq
+         cat += regions[k][1]   # identifier
+
+      pappi = ProteinAnalysis(kitty)
+      print("%s,%.3lf kda" %(cat, pappi.molecular_weight() / 1000))
+   beg += 1
 
 
 
